814072c53f70fef06affb4f32d114c49631cc1fb
Alejandro Cadena <2168579@johnabbottcollege.net>
2022-12-15 18:26:47 -0500

    added time taken to travel

--- remPlanner.js
+++ remPlanner.js
@@ -41,21 +41,58 @@ createOptions();
 
 // getting the beginning and end stations to create the path and calculate time
 
- function getStartAndEndStation()
+ async function getInfo(path){
+    response = await fetch(path);
+    const pathData = response.json()
+
+    return pathData;
+};
+
+
+async function getStartAndEndStation()
 {
     var start = document.getElementById('beginSt');
     var end = document.getElementById('endSt');
 
-    var startValue = start.options[start.selectedIndex].value;
+    var startValue = start.options[start.selectedIndex].text;
 
-    var endValue = end.options[end.selectedIndex].value;
+    var endValue = end.options[end.selectedIndex].text;
 
-    console.log(startValue);
-    console.log(endValue);
+    let completePath = " http://10.101.0.12:8080/path/" + encodeURIComponent(startValue) + "/" + encodeURIComponent(endValue);
     
+   let pathData = await getInfo(completePath);
 
+    let firstStationSegmentId = pathData[0].SegmentId;
+
+   for(segment in pathData)
+   {
+        if(firstStationSegmentId != pathData[segment].SegmentId)
+        {
+            let newStationName = pathData[segment].Name;
+
+            let currentSegmentId = pathData[segment].SegmentId;
+
+            // getting distance
+
+            let distancePath = "http://10.101.0.12:8080/distance/" + encodeURIComponent(startValue) + "/" + encodeURIComponent(newStationName);
+
+
+            let totalDist = await getInfo(distancePath); 
+
+            let speedo = await getInfo("http://10.101.0.12:8080/averageTrainSpeed");
+            
+            let totalSpeed = speedo[0].AverageSpeed;
+
+            let timeToTravel = totalDist / totalSpeed;
+
+            console.log(timeToTravel);
+        }
+   }
 }
 
 var bttn = document.getElementById('print');
 
 bttn.addEventListener("click",  getStartAndEndStation);
+
+
+
